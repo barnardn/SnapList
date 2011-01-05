aa4cc84bd4280a30bf24ca6c637595fb9a587e78
Norm Barnard <barnard@clamdango.com>
2011-01-05 02:32:38 +0000

    Initial import of source

---
+++ ListMonster/ListMonster/Classes/MetaList.m
@@ -0,0 +1,27 @@
+//
+//  List.m
+//  ListMonster
+//
+//  Created by Norm Barnard on 12/27/10.
+//  Copyright 2010 clamdango.com. All rights reserved.
+//
+
+#import "MetaList.h"
+#import "MetaListItem.h"
+#import "Schedule.h"
+#import "NSStringExtensions.h"
+
+@implementation MetaList
+
+@dynamic name, listID, dateCreated, items, schedule, category;
+
+
+#pragma mark -
+#pragma mark NSManagedObject overrides
+
+- (void)awakeFromInsert {
+    [self setDateCreated:[NSDate date]];
+    [self setListID:[NSString stringWithUUID]];
+}
+
+@end
---
+++ ListMonster/ListMonster/Classes/Schedule.m
@@ -0,0 +1,16 @@
+//
+//  Schedule.m
+//  ListMonster
+//
+//  Created by Norm Barnard on 12/27/10.
+//  Copyright 2010 clamdango.com. All rights reserved.
+//
+
+#import "Schedule.h"
+
+
+@implementation Schedule
+
+@dynamic list, period, monday, tuesday, wednesday, thursday, friday, saturday, sunday;
+
+@end
