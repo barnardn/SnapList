c2a3c1a20323f9646e8eaad08d1b847dc6f86e62
Norm Barnard <barnard@clamdango.com>
2011-03-06 23:10:06 +0000

    disabled checked state segment control if no list items

--- ListMonster/Classes/ListItemsViewController.m
+++ ListMonster/Classes/ListItemsViewController.m
@@ -94,10 +94,12 @@
     if ([[[self theList] items] count] == 0) {
         [[[self navigationItem] rightBarButtonItem] setEnabled:NO];
         [[self moreActionsBtn] setEnabled:NO];
+        [[self checkedState] setEnabled:NO];
         return;
     }
     [[[self navigationItem] rightBarButtonItem] setEnabled:YES];
     [[self moreActionsBtn] setEnabled:YES];
+    [[self checkedState] setEnabled:YES];
     [self setListItems:[self itemsSortedBy:byName]];
     [self filterItemsByCheckedState];           // TODO: refactor to take sorted list then assign to datasource
     [editItemNavController release], editItemNavController = nil;
