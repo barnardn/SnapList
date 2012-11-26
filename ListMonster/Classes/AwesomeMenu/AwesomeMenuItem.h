0529fbcc739b2cebe2265f1903e7c470b70fd3db
Norm Barnard <barnard@clamdango.com>
2012-11-26 14:38:37 -0500

    added corner menu to project

---
+++ ListMonster/Classes/AwesomeMenu/AwesomeMenuItem.h
@@ -0,0 +1,50 @@
+//
+//  AwesomeMenuItem.h
+//  AwesomeMenu
+//
+//  Created by Levey on 11/30/11.
+//  Copyright (c) 2011 Levey & Other Contributors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define AwesomeMenuUseDefaultAddButtonKey           @"AwesomeMenuUseDefaultAddButton"
+#define AwesomeMenuNormalBackgroundImageKey         @"AwesomeMenuNormalBackgroundImageKey"
+#define AwesomeMenuHighlightedBackgroundImageKey    @"AwesomeMenuHighlightedBackgroundImageKey"
+#define AwesomeMenuNormalContentImageKey            @"AwesomeMenuNormalContentImageKey"
+#define AwesomeMenuHighlightedContentImageKey       @"AwesomeMenuHighlightedContentImageKey"
+
+@protocol AwesomeMenuItemDelegate;
+
+@interface AwesomeMenuItem : UIImageView
+{
+    UIImageView *_contentImageView;
+    CGPoint _startPoint;
+    CGPoint _endPoint;
+    CGPoint _nearPoint; // near
+    CGPoint _farPoint; // far
+    
+    id<AwesomeMenuItemDelegate> __unsafe_unretained _delegate;
+}
+
+@property (nonatomic, strong) UIImageView *contentImageView;
+
+@property (nonatomic) CGPoint startPoint;
+@property (nonatomic) CGPoint endPoint;
+@property (nonatomic) CGPoint nearPoint;
+@property (nonatomic) CGPoint farPoint;
+
+@property (nonatomic, unsafe_unretained) id<AwesomeMenuItemDelegate> delegate;
+
+- (id)initWithImage:(UIImage *)img 
+   highlightedImage:(UIImage *)himg
+       ContentImage:(UIImage *)cimg
+highlightedContentImage:(UIImage *)hcimg;
+
+
+@end
+
+@protocol AwesomeMenuItemDelegate <NSObject>
+- (void)AwesomeMenuItemTouchesBegan:(AwesomeMenuItem *)item;
+- (void)AwesomeMenuItemTouchesEnd:(AwesomeMenuItem *)item;
+@end
