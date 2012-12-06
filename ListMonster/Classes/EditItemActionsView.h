8f723f6c58e9f16c599eda8c4d88955ad3eb2f9b
Norm Barnard <barnard@clamdango.com>
2012-12-05 20:57:55 -0500

    removed old crappy assets.  added stub EditItemActionsView

---
+++ ListMonster/Classes/EditItemActionsView.h
@@ -0,0 +1,13 @@
+//
+//  EditItemActionsView.h
+//  ListMonster
+//
+//  Created by Norm Barnard on 12/5/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditItemActionsView : UIView
+
+@end
---
+++ ListMonster/Classes/EditItemActionsView.m
@@ -0,0 +1,31 @@
+//
+//  EditItemActionsView.m
+//  ListMonster
+//
+//  Created by Norm Barnard on 12/5/12.
+//
+//
+
+#import "EditItemActionsView.h"
+
+@implementation EditItemActionsView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
